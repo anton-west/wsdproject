3a5b738d05d843fae9ec0fbd5d2d1b3d555c4df7
Anton West <anton.west@netikka.fi>
2020-12-03 20:37:51 +0200

    created views

---
+++ config/config.js
@@ -0,0 +1,11 @@
+let config = {};
+
+config.database = {
+  hostname: "hattie.db.elephantsql.com",
+  database: "ozzkbyhx",
+  user: "ozzkbyhx",
+  password: "fD39BWU8spAVnwFgVT6bHqtvSXTqvN24",
+  port: 5432
+};
+
+export { config }; 
