3f5fec6bb8a909cd54afb94f8a4268cb73745505
Anton West <anton.west@netikka.fi>
2020-12-10 20:25:16 +0200

    test

--- database/database.js
+++ database/database.js
@@ -2,8 +2,7 @@ import { Client } from "../deps.js";
 import { config } from "../config/config.js";
 
 const getClient = () => {
-    const DATABASE_URL = Deno.env.toObject().DATABASE_URL;
-    return new Client(DATABASE_URL);
+    return new Client(config.database);
 }
   
 const executeQuery = async(query, ...args) => {
