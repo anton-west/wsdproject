338f2453aeaaab5be309a1521b7f37196b20ad50
Anton West <anton.west@netikka.fi>
2020-12-10 19:37:23 +0200

    fix5

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 let config = {};
 
 config.database = {
-  hostname: "hattie.db.elephantsql.com",
-  database: "ozzkbyhx",
-  user: "ozzkbyhx",
-  password: "fD39BWU8spAVnwFgVT6bHqtvSXTqvN24",
+  hostname: "ec2-176-34-123-50.eu-west-1.compute.amazonaws.com",
+  database: "da6ivbreqr84nh",
+  user: "xmgqllogmxgmle",
+  password: "7f5b175ed2df4d83d55944b589bd2ff1f19975f4325dbbcf46d0329568473ba7",
   port: 5432
 };
 
