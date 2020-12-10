fb97a2546185e9d18ad8a5c219cada250eabb80d
Anton West <anton.west@netikka.fi>
2020-12-10 22:53:23 +0200

    removed creds

--- config/config.js
+++ config/config.js
@@ -1,11 +1,7 @@
 let config = {};
 
 config.database = {
-  hostname: "hattie.db.elephantsql.com",
-  database: "ozzkbyhx",
-  user: "ozzkbyhx",
-  password: "fD39BWU8spAVnwFgVT6bHqtvSXTqvN24",
-  port: 5432
+
 };
 
 export { config }; 
