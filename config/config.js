2f08d8b61af4229df300ef2d9a400ff91d069264
Anton West <anton.west@aalto.fi>
2020-12-11 20:10:26 +0200

    db cred fix

--- config/config.js
+++ config/config.js
@@ -1,11 +1,15 @@
 let config = {};
 
-config.database = {
-  hostname: "ec2-18-203-7-163.eu-west-1.compute.amazonaws.com",
-  database: "dbe6gkamhig3jc",
-  user: "efxitbikozgxoj",
-  password: "bb89b959b427e5747b4ccc144480278d5194366743917bf470189810483a5326",
-  port: 5432
-};
+if (Deno.env.get('DATABASE_URL')) {
+  config.database = Deno.env.get('DATABASE_URL');
+} else {
+  config.database = {
+    hostname: "",
+    database: "",
+    user: "",
+    password: "",
+    port: ""
+  };  
+}
 
 export { config }; 
