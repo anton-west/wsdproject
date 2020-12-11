64f275bcd91d65525a701f8f4e3dd2920aaa18f9
Anton West <anton.west@netikka.fi>
2020-12-11 21:56:23 +0200

    done

--- config/config.js
+++ config/config.js
@@ -1,7 +1,15 @@
 let config = {};
 
-config.database = {
-
-};
+if (Deno.env.get('DATABASE_URL')) {
+    config.database = Deno.env.get('DATABASE_URL');
+  } else {
+    config.database = {
+        hostname: "",
+        database: "",
+        user: "",
+        password: "",
+        port: ""
+    };  
+  }
 
 export { config }; 
