0be10bc5aebf347b57a45e490d5291a78ff214af
Anton West <anton.west@aalto.fi>
2023-02-10 11:34:21 +0200

    fix

---
+++ config/config.js
@@ -0,0 +1,15 @@
+let config = {};
+
+if (Deno.env.get('DATABASE_URL')) {
+    config.database = Deno.env.get('DATABASE_URL');
+  } else {
+    config.database = {
+        hostname: "",
+        database: "",
+        user: "",
+        password: "",
+        port: ""
+    };  
+  }
+
+export { config }; 
